e3d79ef896103922a448a04e04e747c7c243d992
lukaszmichalskii <lukasz.michalski222@gmail.com>
2022-10-20 13:22:49 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Kernals.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Kernals.dir/main.cpp.o"
+  "Kernals"
+  "Kernals.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Kernals.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
